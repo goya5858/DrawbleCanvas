7aeb776078ba125d9c89f80d5536691aeef56f99
goya5858 <hokuzaki1203@gmail.com>
2021-12-15 00:56:03 +0900

    test axios

--- src/component/FormImg.tsx
+++ src/component/FormImg.tsx
@@ -1,24 +1,50 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
+import axios from 'axios';
 
 const FormImg = () => {
-    const [fileURL, setFileURL] = useState("");
-    function processImg(event: any) {
-        const imgFile = event.target.files[0];
+    const [fileURL, setFileURL] = useState<any>();
+    const [imgData, setImgData] = useState<File>();
+
+
+
+    // 入力画像が変更されたときに、提出用のオブジェクトを変更
+    const handleSetImg = (e: ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return;
+        const imgFile: File = e.target.files[0];
         const imgURL  = URL.createObjectURL(imgFile);
-        console.log("==========", imgURL);
+        setImgData( imgFile );
         setFileURL( imgURL );
     }
+
+    const handleSubmitData = async() => {
+        //const formData = new FormData();
+        if ( !imgData ) return;
+        //formData.append( "submitImg", imgData );
+        const subURL = "https://duw8yp1jr2.execute-api.ap-northeast-1.amazonaws.com/prob";
+        //const headers = { "content-type": "multipart/form-data",};
+        axios({
+            method: 'post',
+            url: subURL,
+            data: fileURL
+        })
+        .then(res => { console.log(res); })
+        .catch(res => { console.log(res) })
+    }
+
     return (
         <div>
-            <form method="post" encType="multipart/form-data" action="https://neozbbmfag.execute-api.ap-northeast-1.amazonaws.com/testFunction">
-                <input type="file" 
-                       name="example" 
-                       accept="image/*" 
-                       onChange={processImg}
-                />
-                <img src={fileURL}/>
-                <button type="submit">submit</button>
-            </form>
+            <input type="file" 
+                   name="example" 
+                   accept="image/*" 
+                   onChange={(e: ChangeEvent<HTMLInputElement>) => handleSetImg(e)}
+            />
+            <input type="text"
+                   onChange={(e: ChangeEvent<HTMLInputElement>) => handleSetImg(e)}
+            ></input>
+            <img src={ fileURL }  alt="description"/>
+            <button type="button"
+                onClick={handleSubmitData}
+            >submit</button>
             
         </div>
     )
