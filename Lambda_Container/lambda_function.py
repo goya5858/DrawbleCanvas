abc3ad94cf7706eee278d5bf68c3e5bc78ecdb1a
goya5858 <hokuzaki1203@gmail.com>
2021-12-17 20:35:48 +0900

    見やすく整理

--- Lambda_Container/lambda_function.py
+++ Lambda_Container/lambda_function.py
@@ -8,69 +8,51 @@ import numpy as np
 import cv2
 
 def lambda_handler(event, context):
-    #filename  = datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + ".txt"
+    print("event-keys: ", event.keys())
     filename  = datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + ".jpg"
     tmp_filename = "/tmp/" + filename
     bucket_name  = "test-bucket-5858"
 
     s3 = boto3.client("s3")
     
-    #print(event)
-    print("event-keys: ", event.keys())
-    #print("event[body]:",  event["body"])
-    
+    ### 送信されたデータの画像データへの読み込み ###
     body = json.loads(event["body"]) # str -> dict
     print("type of event[body]:", type(event["body"]))
-    #print("body:", body)
 
-    content = body["text"]
+    content = body["text"] # 送信時に"text"にデータを挿入しているため
     print("size of content:", sys.getsizeof(body["text"]))
-    #print("content:", content)
 
     text64 = content.split(",")[1] #Base64のテキストデータの必要部分だけ取り出し
     print("num of base64-sep is", len(content.split(",")))
-    #print("text64:", text64)
 
     text_byte = base64.b64decode(text64) #byteデータに変換
-    #print("text_byte:", text_byte)
     print("success convert to Byte_data")
-
-    ##### TEXTデータではこっちを使用 ######
-        #text_raw = text_byte.decode() #Byteデータをデコード
-        ##print("text_raw:", text_raw)
     
     jpg=np.frombuffer(text_byte,dtype=np.uint8)
     print("success convert to numpy array")
-    #print(jpg)
+
     img = cv2.imdecode(jpg, cv2.IMREAD_COLOR)
     print("success convert to IMG by imdecode")
     print("type of img:", type(img))
 
-    #with open(tmp_filename, "w") as f:
-    #    f.write(text_raw)
-    cv2.imwrite(tmp_filename,img) # jpgファイルの保存
-    print("success img save to /tmp/")
+    ### 読み込まれた画像データの保存 ###
+    cv2.imwrite(tmp_filename,img) 
+    print("success save img to /tmp/")
     print(os.listdir("/tmp/"))
     
+    ### 画像データのBase64(str)へのエンコード ###
     encode_parms  = [int(cv2.IMWRITE_WEBP_QUALITY), 50]
-    ret, enimg = cv2.imencode('.webp', img, encode_parms)
+    ret, encode_img = cv2.imencode('.webp', img, encode_parms)
     print("encode img")
-    print("type of ret & enimg:", type(ret), type(enimg) )
+    print("type of ret & enimg:", type(ret), type(encode_img) )
+
+    body = base64.b64encode(encode_img).decode('utf-8')
+    print("Encode return IMG")
+    print("type of body:", type(body))
 
     try: 
-        # 画像のアップロード
+        # S3へ画像のアップロード
         s3.upload_file(tmp_filename, bucket_name, filename)
-
-        # アップロードした画像を読み込んでクライアントに返す
-        #response = s3.get_object(
-        #        Bucket=bucket_name,
-        #        Key=filename,
-        #        )
-        #res_img  = response['Body'].read()
-        #print("type of res_img:", type(res_img))
-        body = base64.b64encode(enimg).decode('utf-8')
-        print("Encode return IMG")
-        print("type of body:", type(body))
     
         return {
             'statusCode': 200,
--- src/component/PostImgFile.tsx
+++ src/component/PostImgFile.tsx
@@ -2,29 +2,20 @@ import axios from 'axios';
 import {ChangeEvent, useState, useEffect} from "react";
 
 const PostImgFile = () => {
-  //const [rawData,    setRawData]    = useState<File>();
-  const [submitData, setSubmitData] = useState<string>();
-  const [imgURL,     setImgURL]     = useState<string>();
-  const [replyImgURL,setReplyImgURL]   = useState<string>();
-  //let new_Img = new Image();
+  const [submitData, setSubmitData]  = useState<string>();
+  const [imgURL,     setImgURL]      = useState<string>();
+  const [replyImgURL,setReplyImgURL] = useState<string>();
 
   useEffect( ()=>{
-    //console.log("submitData: ", submitData);
     console.log("IMG Loaded")
   }, [submitData] );
 
   // Inputが変更されるたびに表示&提出用のデータを更新
   const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
-    //console.log( typeof e.target.value );
     const _rawdata: File = e.target.files[0] //提出する生データ
     const imgURL:string  = URL.createObjectURL(e.target.files[0]);
     setImgURL( imgURL ); //読み込んだ画像を表示
-
-    //// 生のtextデータをファイルデータへ変換 (確認のため)
-    //const _rawdata = new File( [ e.target.value] , "text.txt",
-    //                 {  type: "text/plain",
-    //                    lastModified: 0} );
 
     // ファイルデータをBase64でエンコードして、submit_dataにセットする
     if (!_rawdata) return ; 
@@ -34,28 +25,21 @@ const PostImgFile = () => {
                   text: base64file,
                 } )
     setSubmitData( _submitdata );
-
-    //console.log( "e.target.files[0]: ", e.target.files[0] );
-    //console.log("_rawdata:", _rawdata);
-    //console.log("imgURL: ", imgURL)
-    //console.log("_submit-data:", _submitdata)
-    //console.log("submit-data:", submitData)
   }
 
   const handleSubmitData = async() => {
-    axios({
-      method: 'post',
-      url:    'https://txsei0q801.execute-api.ap-northeast-1.amazonaws.com/default/axiosFunction2',
-      data:   submitData,
-    })
-    .then(res => {
-      console.log(res)
-      //new_Img.src = res.data;
-      let new_Img:File      = createJpegFile4Base64( res.data, "new_img" )
-      let newImgURL:string  = URL.createObjectURL( new_Img );
-      setReplyImgURL( newImgURL );
-      console.log(new_Img);
-    })
+    axios( {
+             method: 'post',
+             url:    'https://txsei0q801.execute-api.ap-northeast-1.amazonaws.com/default/axiosFunction2',
+             data:   submitData,
+          } )
+    .then( res => {
+            console.log(res)
+            let new_Img:File      = createJpegFile4Base64( res.data, "new_img" )
+            let newImgURL:string  = URL.createObjectURL( new_Img );
+            setReplyImgURL( newImgURL );
+            console.log(new_Img);
+          } )
     .catch(results => {
       console.log(results);
     });
